7e4cbe68c509f866f2b6b8a08af8c05ecf3005ca
Yorchrr <iorchrr@gmail.com>
2025-08-08 18:53:56 -0600

    Se realiza avance al codigo

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# Titulo de mi proyecto
+# Proyecto de Srpint 7
 Mi proyecto sprint 7 de aplicación dentro de Tripleten
+Este proyecto utiliza Python, Streamlit y Plotly-express para crear una aplicación interactiva que analiza la data de Vehiculos.
 
+## Requisitos
+- Python 3.10 o superior
+- Streamlit
+- Plotly-express
+
+## Instalación
+pip install -r requirements.txt
+
+## Uso
+streamlit run app.py
+
+## Estructura del proyecto
+├── app.py              # Aplicación principal de Streamlit
+├── notebooks           # Datos de entrada
+├── README.md           # Documentación del proyecto
+└── requirements.txt    # Requerimientos
+
+## Creditos
+Jorge Arturo Reyna Rosas - Data Analyst Tripleten
+Ricardo Huilipan - Coach Tripleten
--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 import pandas as pd
-
 import streamlit as st
+import plotly.express as px
 
-st.header('Lanzar una moneda')
+df = pd.read_csv("vehicles_us.csv")
 
-st.write('Esta aplicación aún no es funcional. En construcción.')
-print("Hola Mundo")
-print('Hola Terrícolas')
+st.header("Este es un encabezado interactivo")
+
+hist_button = st.button('Crear histograma') #crear un botón
+     
+if hist_button:#al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         
+    # crear un histograma
+    fig = px.histogram(df, x="odometer")
+     
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Crear dispersion')
+
+if disp_button:
+    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(df, x="odometer", y="price") # crear un gráfico de dispersión
+    st.plotly_chart(fig, use_container_width=True)
+
+
 
 
